812aefae81cb056d164a131384973a1c2d224041
Paulo <pigenio@gmail.com>
2013-05-06 21:16:37 -0600

    Added toggle image + security fixes.

--- wp-content/themes/t99/functions.php
+++ wp-content/themes/t99/functions.php
@@ -2,7 +2,27 @@
 
 add_theme_support( "post-thumbnails" );
 
-remove_action('wp_head', 'wp_generator');
+function wordpress_head_cleanup() {
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+  remove_action('wp_head', 'index_rel_link');
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+
+  global $wp_widget_factory;
+  remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+
+  add_filter('use_default_gallery_style', '__return_null');
+}
+
+add_action('init', 'wordpress_head_cleanup');
+
+define( 'DISALLOW_FILE_EDIT', true );
 
 function t99_init () {
 

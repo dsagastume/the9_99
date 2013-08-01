864bf5b265424cbd646f4ead1d58ebd4b37c305e
Diego Sagastume <dsagastume.castillo@gmail.com>
2013-07-31 22:08:17 -0600

    Updated and fixed styles + "publications".

---
+++ publications/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>The 9.99 Gallery | Publications</title>
+
+<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,600' rel='stylesheet' type='text/css'>
+<link href="../style.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="../modernizr.js"></script>
+<script type="text/javascript" src="../global.js"></script>
+
+<?php include "../header.php" ?>
+
+<div class="content">
+<h1>publications</h1>
+
+<div class="subcontent publications">
+
+<div class="publication">
+<div>
+<img src="singular_plurality.jpg">
+</div>
+<div class="publication-text">
+<p><a>A Singular Plurality <span aria-hidden="true" data-icon="&#xe003;"></span></a></p>
+<p>
+
+david rockefeller center for latin american studies DRCLAS, harvard university<br>
+
+cambridge, massachusetts, 2012<br>
+
+texts by: thomas b.f. cummins, kevin power, doris sommer, christian viveros-faune and interview by alma ruiz.<br>
+
+
+</p>
+</div>
+</div>
+
+<div class="publication">
+<div style="float:left;">
+<img src="objetos_en_transito.jpg">
+</div>
+<div style="float:left; padding:0 10px;">
+<p><a>Objetos En Tránsito <span aria-hidden="true" data-icon="&#xe003;"></span></a></p>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
